0f59d65228bda15d2cb03a197084de3211e6be48
abdallamfathy <abdallamfathy@gmail.com>
2022-11-14 10:16:14 +0200

    project structure

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Main from "./Main";
+import Projects from "./Projects";
+import Products from "./Products";
+import Social from "./Social";
+import About from "./About";
+
+export  { Contact, Footer, Navbar, Main, Projects, Products, Social, About };
---
+++ tst
@@ -0,0 +1,6 @@
+[1] make routes
+[2] make components
+[3] make all container
+[4] start with navbar
+[5] slider
+[6] sm icons
