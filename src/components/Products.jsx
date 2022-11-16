80b79bea295c223ce9f04f2efd293e5469f394ec
abdallamfathy <abdallamfathy@gmail.com>
2022-11-16 16:33:18 +0200

    edit products transition

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -9,11 +9,11 @@ const Products = () => {
           
       <h1 className='text-4xl text-[#3A3535] font-bold text-center mb-6'>منتجاتنا</h1>
       <div className='flex flex-wrap flex-row justify-center gap-20 gap-x-24'>
- <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+ <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-in-out hover:w-72  w-44 h-44 hover:h-72   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
@@ -22,201 +22,163 @@ const Products = () => {
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
+<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
+<a href="#" className=' z-50 '>
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
 </a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
+<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
   <p class=" ">
     المقاس : 20 × 20 × 40
   </p>
   <p>لون الشريحة : بدون</p>
-
 </div>
 </div>
 
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
-</a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
-  <p>النوع :3 فتحات</p>
-  <p class=" ">
-    المقاس : 20 × 20 × 40
-  </p>
-  <p>لون الشريحة : بدون</p>
 
-</div>
-</div>
-
-<div className=" flex flex-col justify-center items-center w-[305px] h-[342px] bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700 ">
-<a href="#">
-  <img className="-my-10 " src={brick1} alt="asd" />
-</a>
-<div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] ">
-  <p>النوع :3 فتحات</p>
-  <p class=" ">
-    المقاس : 20 × 20 × 40
-  </p>
-  <p>لون الشريحة : بدون</p>
-
-</div>
-</div>
 </div> 
      <Button text={"عرض المزيد"}/>
         </div>
