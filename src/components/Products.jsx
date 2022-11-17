2c9c0a68b3f46ce326e08ed78416829ec4389761
abdallamfathy <abdallamfathy@gmail.com>
2022-11-17 15:42:56 +0200

    add new images to products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
 import brick1 from '../assets/brick1.png'
+import brick2 from '../assets/brick2.png'
+import brick3 from '../assets/brick3.png'
+import brick4 from '../assets/brick4.png'
+import brick5 from '../assets/brick5.png'
+import brick6 from '../assets/brick6.png'
+import brick7 from '../assets/brick7.png'
+import brick8 from '../assets/brick8.png'
+import brick9 from '../assets/brick9.png'
+import brick10 from '../assets/brick10.png'
+import brick11 from '../assets/brick11.png'
+import brick12 from '../assets/brick12.png'
+
+
 import Button from './Button'
 
 const Products = () => {
@@ -24,7 +37,7 @@ const Products = () => {
 
 <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick2} alt="asd" />
 </a>
 <div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
@@ -37,7 +50,7 @@ const Products = () => {
 
 <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick4} alt="asd" />
 </a>
 <div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
@@ -50,7 +63,7 @@ const Products = () => {
 
 <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick5} alt="asd" />
 </a>
 <div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
@@ -63,7 +76,7 @@ const Products = () => {
 
 <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick6} alt="asd" />
 </a>
 <div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
@@ -76,7 +89,7 @@ const Products = () => {
 
 <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick7} alt="asd" />
 </a>
 <div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
@@ -89,7 +102,7 @@ const Products = () => {
 
 <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick8} alt="asd" />
 </a>
 <div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
@@ -102,7 +115,7 @@ const Products = () => {
 
 <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick9} alt="asd" />
 </a>
 <div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
@@ -115,7 +128,7 @@ const Products = () => {
 
 <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick10} alt="asd" />
 </a>
 <div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
@@ -128,7 +141,7 @@ const Products = () => {
 
 <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick11} alt="asd" />
 </a>
 <div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
@@ -141,7 +154,7 @@ const Products = () => {
 
 <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick11} alt="asd" />
 </a>
 <div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
@@ -154,7 +167,7 @@ const Products = () => {
 
 <div className=" flex flex-col justify-center items-center w-[305px] h-[342px] relative group bg-white rounded-lg border border-gray-50 shadow-lg  dark:bg-gray-800 dark:border-gray-700">
 <a href="#" className=' z-50 '>
-  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick1} alt="asd" />
+  <img className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out hover:left-20 absolute left-24 top-12  hover:w-44  w-36 h-36 hover:h-44   " src={brick12} alt="asd" />
 </a>
 <div className="text-right text-[#3A3535] text-[24px] h-[108px] w-[250px] relative top-14 ">
   <p>النوع :3 فتحات</p>
