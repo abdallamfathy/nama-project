7e67b9b82ea258f92d4de62a79c146f5e6386e87
abdallamfathy <abdallamfathy@gmail.com>
2022-11-20 16:55:53 +0200

    edit search border

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,7 +63,7 @@ const Navbar = ({textColor , socialDisplay}) => {
                     <div class="flex absolute inset-y-0 right-0 items-center pr-6 pointer-events-none">
                         <svg aria-hidden="true" class="w-5 h-5 text-[#FF7315] " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                     </div>
-                    <input type="search" id="default-search" class="block p-4 pr-10 w-[310px] h-[50px] text- indent-48 text-sm text-gray-900 bg-gray-50 rounded-lg border-gray-50 " placeholder="...بحث" required=""/>
+                    <input type="search" id="default-search" class="block p-4 pr-10 w-[310px] h-[50px] text- indent-48 text-sm text-gray-900 bg-gray-50 rounded-lg border-[#ff73155e] " placeholder="...بحث" required=""/>
                     
                 </div>
                 <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
