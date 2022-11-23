de3c1bb5d71df68ac8ae4e92b7f33367d0907705
abdallamfathy <abdallamfathy@gmail.com>
2022-11-23 13:21:50 +0200

    add products pages roots

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import AboutPage from './pages/AboutPage'
 import ProductsPage from './pages/ProductsPage'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import QualityPage from './pages/QualityPage'
+import ProductsWhite from './pages/ProductsWhite'
+import ProductsNormal from './pages/ProductsNormal'
+import ProductsBlue from './pages/ProductsBlue'
 
 function App() {
 
@@ -18,6 +21,10 @@ function App() {
       <Route  path="/about"  element={<AboutPage/>} />
       <Route  path="/products"  element={<ProductsPage/>} />
       <Route  path="/quality"  element={<QualityPage/>} />
+      <Route  path="/products/white"  element={<ProductsWhite/>} />
+      <Route  path="/products/normal"  element={<ProductsNormal/>} />
+      <Route  path="/products/blue"  element={<ProductsBlue/>} />
+
       </Routes>
       </Router>
     </div>
