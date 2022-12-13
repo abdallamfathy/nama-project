9d8da18543c06e80bee2892737f117b99b017cb4
abdallamfathy <abdallamfathy@gmail.com>
2022-12-13 13:39:25 +0200

    edit navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import './index.css'
-import  {Main, Navbar}  from './components'
-import { Footer } from 'flowbite-react'
-import PartnersPage from './pages/PartnersPage'
-import AboutPage from './pages/AboutPage'
-import ProductsPage from './pages/ProductsPage'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import QualityPage from './pages/QualityPage'
-import ProductsWhite from './pages/ProductsWhite'
-import ProductsNormal from './pages/ProductsNormal'
-import ProductsBlue from './pages/ProductsBlue'
+import "./index.css";
+import { Main, Navbar } from "./components";
+import PartnersPage from "./pages/PartnersPage";
+import AboutPage from "./pages/AboutPage";
+import ProductsPage from "./pages/ProductsPage";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import QualityPage from "./pages/QualityPage";
+import ProductsWhite from "./pages/ProductsWhite";
+import ProductsNormal from "./pages/ProductsNormal";
+import ProductsBlue from "./pages/ProductsBlue";
 
 function App() {
-
   return (
-    <div className="App" id='app'>
+    <div className="App" id="app">
       <Router>
         <Routes>
-      <Route  path="/"  element={<Main/>} />
-      <Route  path="/partners"  element={<PartnersPage/>} />
-      <Route  path="/about"  element={<AboutPage/>} />
-      <Route  path="/products"  element={<ProductsPage/>} />
-      <Route  path="/quality"  element={<QualityPage/>} />
-      <Route  path="/products/white"  element={<ProductsWhite/>} />
-      <Route  path="/products/normal"  element={<ProductsNormal/>} />
-      <Route  path="/products/blue"  element={<ProductsBlue/>} />
-
-      </Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/partners" element={<PartnersPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/quality" element={<QualityPage />} />
+          <Route path="/products/white" element={<ProductsWhite />} />
+          <Route path="/products/normal" element={<ProductsNormal />} />
+          <Route path="/products/blue" element={<ProductsBlue />} />
+        </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
