6dac69250a6beecef96b0db0c214e8d1a42c4685
abdallamfathy <abdallamfathy@gmail.com>
2022-12-18 13:43:25 +0200

    edit certificates compoinent

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,18 +1,33 @@
 import React from "react";
-import cert4 from "../assets/cert4.png";
-import cert5 from "../assets/cert5.png";
-import cert6 from "../assets/cert6.png";
+import exlogo from "../assets/ex-logo.png";
+import inlogo from  "../assets/in-logo.png";
+import hrlogo from "../assets/hr-logo.png"
+import justicelogo from "../assets/justice-logo.png"
+import citylogo from "../assets/cities-logo.png"
+import houselogo from "../assets/house-logo.png"
+import tradelogo from "../assets/trade-logo.png"
+import energylogo from "../assets/energy-logo.png"
+
+
+
 
 const Certificates = () => {
   return (
     <div className="my-28">
-      <div className="flex justify-center my-8">
-        <h1 className="text-4xl font-bold  relative ">شهادات واعتمادات</h1>
+      <div className="flex flex-col items-center justify-center my-8">
+        <h1 className="text-4xl font-bold  relative mb-10">شهادات واعتمادات</h1>
       </div>
-      <div className="flex justify-center ">
-        <img src={cert4} alt="image" />
-        <img src={cert5} alt="image" />
-        <img src={cert6} alt="image" />
+        <p className="text-right mb-10 mr-60 text-xl ">مصنع نما طيبة حاصل على جميع الشهادات والاعتمادات من الجهات السعودية المختصة.</p>
+      <div className="flex justify-center flex-wrap gap-44 ">
+        <img src={inlogo} alt="image" />
+        <img src={hrlogo} alt="image" />
+        <img src={exlogo} alt="image" />
+        <img src={justicelogo} alt="image"/>
+        <img src={citylogo} alt="image" />
+        <img src={houselogo} alt="image" />
+        <img src={tradelogo} alt="image" />
+        <img src={energylogo} alt="image" />
+
       </div>
     </div>
   );
