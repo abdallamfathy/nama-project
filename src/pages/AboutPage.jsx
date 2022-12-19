1d594664bd5f5cc635238ba6b96500f31af531cb
abdallamfathy <abdallamfathy@gmail.com>
2022-12-19 14:15:10 +0200

    edit about page px

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -40,30 +40,30 @@ const AboutPage = () => {
 
           </p>
         </div>
-        <div className="flex flex-row flex-wrap 2xl:gap-8 gap-8 justify-center">
+        <div className="flex flex-row flex-wrap 2xl:gap-8 gap-16 justify-center">
           <div>
-            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 2xl:left-96 left-72 shadow-xl">
+            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[400px] shadow-xl">
               <p className="text-center text-lg">قيمنا</p>
             </div>
-            <div className="2xl:w-[550px] w-[400px] rounded-2xl p-9 h-72 2xl:text-xl text-sm bg-[#3A3535] inline-block text-white text-right">
+            <div className="2xl:w-[550px] w-[500px] rounded-2xl p-9 h-72 2xl:text-xl text-lg bg-[#3A3535] inline-block text-white text-right">
               <p className="mt-2">رضا عملائنا , الجودة الفائقة الاحترافية, المصداقية والأمانة, الشفافية والوضوح, الابتكار والابداع, تطوير مهارات الكادر الفني والإداري بالمصنع
               </p>
             </div>
           </div>
           <div>
-            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 2xl:left-96 left-72 shadow-xl">
+            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[400px] shadow-xl">
               <p className="text-center text-lg">أهدافنا</p>
             </div>
-            <div className="2xl:w-[550px] w-[400px] rounded-2xl p-9 h-72 2xl:text-xl text-sm bg-[#3A3535] inline-block text-white text-right ">
+            <div className="2xl:w-[550px] w-[500px] rounded-2xl p-9 h-72 2xl:text-xl text-lg bg-[#3A3535] inline-block text-white text-right ">
               <p className="mt-2">إيجاد بيئة عمل متكاملة, نستثمر من خلالها أعلى الكفاءات والخبرات البشرية, لمواكبة احتياجات ومتطلبات السوق السعودي والعربي بشكل عام. مع مسيرة التطوير والتنمية التي تشهدها المملكة بكافة مناحي الحياة والمجتمع ومصممون على التميز فى كل مجالات أعمالنا وفق فلسفة تنتهج الجودة المتكاملة باستخدام ما نملكه من قدرات فنية وموارد بشرية والاستعانة بأحدث ما توصلت اليه التكنولوجيا الحديثة من برامج.
               </p>
             </div>
           </div>
           <div>
-            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 2xl:left-96 left-72 shadow-xl">
+            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[400px] shadow-xl">
               <p className="text-center text-lg">سياستنا الخاصة</p>
             </div>
-            <div className="2xl:w-[550px] w-[400px] rounded-2xl  p-9 h-72 2xl:text-xl text-sm bg-[#3A3535] inline-block text-white text-right">
+            <div className="2xl:w-[550px] w-[500px] rounded-2xl  p-9 h-72 2xl:text-xl text-lg bg-[#3A3535] inline-block text-white text-right">
               <p className="mt-2">رضا عملائنا
 المعيار الوحيد المعتمد لدينا هو رضا العملاء
 التنمية
@@ -74,10 +74,10 @@ const AboutPage = () => {
             </div>
           </div>
           <div>
-            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 2xl:left-96 left-72 shadow-xl">
+            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[400px] shadow-xl">
               <p className="text-center text-lg">رؤيتنا</p>
             </div>
-            <div className="2xl:w-[550px] w-[400px] rounded-2xl p-9 h-72 2xl:text-xl text-sm bg-[#3A3535] inline-block text- text-white text-right ">
+            <div className="2xl:w-[550px] w-[500px] rounded-2xl p-9 h-72 2xl:text-xl text-lg bg-[#3A3535] inline-block text- text-white text-right ">
               <p className="mt-2">التخطيط الناجح والتنفيذ الدقيق والأداء المبتكر والجودة العالية والمصداقية الكبيرة, هى رؤيتنا فى عملنا التي تعزز ريادتنا للخدمات المتكاملة فى مجال المقاولات العامة مع التزامنا على استدامة تنفيذ أعمالنا بكل دقة ومهنية واحترافية, مع مراعاة ثقة عملائنا الغالية, ونعمل على تقديم أفضل الخدمات لها بكل صدق وإخلاص وأمانة.
               </p>
             </div>
