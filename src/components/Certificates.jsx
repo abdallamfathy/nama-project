8ab1554146837cae73e5c84338651bfd82cddf19
abdallamfathy <abdallamfathy@gmail.com>
2022-12-19 11:44:49 +0200

    edit cert px

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -13,20 +13,20 @@ import energylogo from "../assets/energy-logo.png"
 
 const Certificates = () => {
   return (
-    <div className="2xl:my-28 my-20 container mx-auto">
+    <div className="2xl:my-28 my-20 mx-20">
       <div className="flex flex-col items-center justify-center my-8">
         <h1 className="2xl:text-4xl text-3xl font-bold  relative 2xl:mb-10">شهادات واعتمادات</h1>
       </div>
-        <p className="text-right mb-10 mr-60 2xl:text-xl text-lg ">مصنع نما طيبة حاصل على جميع الشهادات والاعتمادات من الجهات السعودية المختصة.</p>
-      <div className="flex justify-center flex-wrap 2xl:gap-44 gap-6 mx-10 ">
-        <img src={inlogo} alt="image" />
-        <img src={hrlogo} alt="image" />
-        <img src={exlogo} alt="image" />
-        <img src={justicelogo} alt="image"/>
-        <img src={citylogo} alt="image" />
-        <img src={houselogo} alt="image" />
-        <img src={tradelogo} alt="image" />
-        <img src={energylogo} alt="image" />
+        <p className="text-right mb-10 2xl:mr-60 2xl:text-xl text-lg ">مصنع نما طيبة حاصل على جميع الشهادات والاعتمادات من الجهات السعودية المختصة.</p>
+      <div className="flex justify-center flex-wrap 2xl:gap-44 gap-60  ">
+        <img src={inlogo} alt="image" className="w-52 h-44" />
+        <img src={hrlogo} alt="image"  className="w-52 h-44" />
+        <img src={exlogo} alt="image" className="w-52 h-44" />
+        <img src={justicelogo} alt="image" className="w-52 h-44"/>
+        <img src={houselogo} alt="image"  className="w-52 h-44"/>
+        <img src={tradelogo} alt="image"  className="w-52 h-44"/>
+        <img src={energylogo} alt="image"  className="w-52 h-44"/>
+        <img src={citylogo} alt="image"  className="w-52 h-44"/>
 
       </div>
     </div>
