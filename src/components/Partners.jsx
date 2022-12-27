0d0f222ca2ea29b42e300d44037bec65460be06c
abdallamfathy <abdallamfathy@gmail.com>
2022-12-27 15:32:44 +0200

    change partners

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import cert1 from "../assets/cert1.png";
-import cert2 from "../assets/cert2.png";
-import cert3 from "../assets/cert3.png";
+import mohaidlogo from "../assets/mohaid-logo.png";
+import mezlogo from  "../assets/mez-logo.png";
+import typologo from "../assets/typo-logo.png"
+import arablogo from "../assets/arab-logo.png"
+import bonlogo from "../assets/bon-logo.png"
+
 import Button from "./Button";
 
 const Partners = () => {
@@ -10,10 +13,13 @@ const Partners = () => {
       <div className="flex justify-center my-8">
         <h1 className="2xl:text-4xl text-3xl font-bold  relative ">شركاء النجاح</h1>
       </div>
-      <div className="flex justify-center ">
-        <img src={cert3} alt="image" />
-        <img src={cert2} alt="image" />
-        <img src={cert1} alt="image" />
+      <div className="flex justify-center flex-wrap 2xl:gap-44 gap-x-40 gap-24  ">
+        <img src={mohaidlogo} alt="image" className="w-44 h-40" />
+        <img src={mezlogo} alt="image"  className="w-48 h-40" />
+        <img src={typologo} alt="image"  className="w-44 h-40"/>
+        <img src={arablogo} alt="image"  className="w-44 h-40"/>
+        <img src={bonlogo} alt="image"  className="w-48 h-40"/>
+
       </div>
       <Button text={"عرض المزيد"} href={"/partners"} />
     </div>
