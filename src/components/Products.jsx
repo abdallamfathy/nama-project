36f439a30cf392df33e57735d93fb08691b75cdf
abdallamfathy <abdallamfathy@gmail.com>
2022-12-29 11:31:20 +0200

    make modal dynamic

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -24,7 +24,7 @@ const Products = () => {
   const name = products.map((product) => product.name);
   const size = products.map((product) => product.size);
   const color = products.map((product) => product.color);
-  console.log(name, size, color);
+  // console.log(name, size, color);
 
   const [showModal, setShowModal] = React.useState(false);
 
@@ -58,44 +58,101 @@ const Products = () => {
     return () => document.removeEventListener("mousedown", closeModal);
   }, [setShowModal]);
 
+  //id of product
+  const [id, setId] = useState(0);
+
+  // const filterProducts = products.filter((product) => {
+  //   if (product.id !== id) {
+  //     return product;
+  //   }
+
   return (
     <div id="products">
       
-      {/* {showModal && (
+      {showModal && (
         <>
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-hidden cursor-pointer  fixed inset-0 z-50 outline-none focus:outline-none overflow-hidden "
             ref={modalRefs}
           >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div className=" border-0 rounded-[60px] shadow-black shadow-2xl relative  flex flex-col w-full bg-white outline-none focus:outline-none">
-                <div className="flex flex-col  justify-center  items-center w-[590px] h-[550px] ">
-                  <img
-                    className="w-[530px] h-[351px] relative top-  "
-                    src={brick1}
-                    alt="Ado"
-                  />
-                  <div className="text-right mx-24 gap-y-4 -mt-20 flex flex-col">
-                    <h1 className="text-2xl text-[#FF7315] font-bold">
-                      بلك بركانى خفيف 3 فتحات
-                    </h1>
-                    <p className="text-xl font-semibold">
-                      مواد خام خفيفة الكثافة
-                    </p>
-                    <p>
-                      البلك المعزول هو مثالى للبناء من جميع النواحى فهو فعال من
-                      حيث التكلفة وسهولة الاستخدام ويوفر خلفية مثالية لقبول
-                      أعمال اللياثة
-                    </p>
-                    <p className="font-bold">متوفر بأشكال مختلفة</p>
-                  </div>
+            
+          {products.filter((product) => (
+          product.id === id
+           )).map((product) => (
+            <div
+              onClick={() => {
+                closeModal, setShowModal(true);
+              }}
+              className=" flex flex-col cursor-pointer justify-between items-center 2xl:w-[305px] 2xl:h-[342px] w-[270px] h-80 relative group  rounded-3xl bg-white shadow-lg  "
+            >
+        <div className="h-44 w-full ">
+          
+        <Swiper
+        modules={[
+          Navigation,
+          Pagination,
+          Scrollbar,
+          A11y,
+          EffectFade,
+          Autoplay,
+        ]}
+        pagination={{
+          dynamicBullets: false,
+          clickable: true,
+          background: "red",
+        }}
+        effect={"fade"}
+        navigation
+        // speed={2500}
+        // autoplay={{ delay: 1500, disableOnInteraction: false }}
+        
+      >
+        <SwiperSlide className="rounded-t-3xl">
+        <div className=" h-32 w-full flex justify-center items-center"> 
+        <img
+                className="transition duration-[2000]  delay-300 hover:transition-all ease-out 2xl:hover:left-20  absolute    hover:w-44  w-36 h-28 hover:h-44 object-contain  "
+                src={product.image}
+                alt="productImage"
+              />
+
+        </div>
+        </SwiperSlide>
+        <SwiperSlide className="rounded-t-3xl">
+        <div className=" h-32 pt-1 w-full flex justify-center items-center  "> 
+        <img
+                className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out 2xl:hover:left-20  absolute 2xl:left-24  2xl:top-12   hover:w-44  w-36 h-28 hover:h-44  rotate-90 object-contain"
+                src={product.image}
+                alt="productImage"
+              />        
+        </div>
+        </SwiperSlide>
+        
+      </Swiper>
+        </div>
+        
+              
+              <div className="text-right  2xl:text-[24px] text-sm h-28 w-full rounded-b-lg text-[#3A3535] flex flex-col  gap-2 py-2  ">
+                
+                
+                <div className="mx-4 flex flex-col justify-center items-end gap-1   text-sm">
+                <p className="text-[#FF7315] text-lg">{product.name}</p>
+                <p>{product.type}</p>
+                <p>{product.size}</p>
+                  <h2 className="font-semibold">{product.use}</h2>
+                  
                 </div>
               </div>
             </div>
+            
+           )
+)}
+        
           </div>
           <div className="opacity-60 fixed inset-0 z-40 bg-black overflow-hidden"></div>
         </>
-      )} */}
+      )}
+
+
 
       <div className="mx-20 my-20 ">
         <h1 className="text-4xl text-[#3A3535] font-bold text-center mb-10">
@@ -105,7 +162,7 @@ const Products = () => {
           {products.map((product) => (
             <div
               onClick={() => {
-                closeModal, setShowModal(true);
+                closeModal, setShowModal(true) , setId(product.id), console.log(id);;
               }}
               className=" flex flex-col cursor-pointer justify-between items-center 2xl:w-[305px] 2xl:h-[342px] w-[270px] h-96 relative group  rounded-lg border border-gray-50 shadow-lg  "
             >
@@ -141,11 +198,7 @@ const Products = () => {
                 src={product.image}
                 alt="productImage"
               />
-        {/* <img
-                className="-my-10 transition duration-[2000]  delay-300 hover:transition-all ease-out 2xl:hover:left-20 hover:left-16 absolute 2xl:left-24 left-20 2xl:top-12 top-10  hover:w-44  w-36 h-36 hover:h-44"
-                src={product.image}
-                alt="productImage"
-              /> */}
+
         </div>
         </SwiperSlide>
         <SwiperSlide>
