d3c1a5ca702a9a6b8d2248f209dc0d3a746a8e95
abdallamfathy <abdallamfathy@gmail.com>
2023-01-01 16:37:56 +0200

    edit about us page 24

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -35,9 +35,9 @@ const AboutPage = () => {
 
           </p>
         </div>
-        <div className="flex flex-row flex-wrap 2xl:gap-28 gap-16 justify-center">
+        <div className="flex flex-row flex-wrap 2xl:gap-28 gap-20 gap-x-64 justify-center">
           <div>
-            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[400px] shadow-xl">
+            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[330px] shadow-xl">
               <p className="text-center text-lg">قيمنا</p>
             </div>
             <div className="2xl:w-[550px] w-[442px] h-96 rounded-2xl p-9  2xl:text-xl text-lg bg-[#3A3535] inline-block text-white text-right">
@@ -46,7 +46,7 @@ const AboutPage = () => {
             </div>
           </div>
           <div>
-            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[400px] shadow-xl">
+            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[330px] shadow-xl">
               <p className="text-center text-lg">أهدافنا</p>
             </div>
             <div className="2xl:w-[550px] w-[442px] h-96 rounded-2xl p-9  2xl:text-xl text-lg bg-[#3A3535] inline-block text-white text-right ">
@@ -55,7 +55,7 @@ const AboutPage = () => {
             </div>
           </div>
           <div>
-            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[400px] shadow-xl">
+            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[330px] shadow-xl">
               <p className="text-center text-lg">سياستنا الخاصة</p>
             </div>
             <div className="2xl:w-[550px] w-[442px] h-96 rounded-2xl  p-9 2xl:text-xl text-lg bg-[#3A3535] inline-block text-white text-right">
@@ -69,7 +69,7 @@ const AboutPage = () => {
             </div>
           </div>
           <div>
-            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[400px] shadow-xl">
+            <div className="bg-white text-[#FF7315] z-100 flex justify-center items-center p- 2xl:w-36 2xl:h-20 w-28 h-16 font-bold text-2xl text-center rounded-xl relative top-8 left-[330px] shadow-xl">
               <p className="text-center text-lg">رؤيتنا</p>
             </div>
             <div className="2xl:w-[550px] w-[442px] h-96 rounded-2xl p-9 2xl:text-xl text-lg bg-[#3A3535] inline-block text- text-white text-right ">
@@ -78,12 +78,11 @@ const AboutPage = () => {
             </div>
           </div>
         </div>
-        <Certificates/>
-        {/* <div className="flex flex-col items-center justify-center my-9">
-        <h1 className="2xl:text-4xl text-3xl font-bold  relative 2xl:mb-10 mt-14">شهادات واعتمادات</h1>
+        <div className="flex flex-col items-center justify-center my-8">
+        <h1 className="2xl:text-5xl text-4xl font-bold  relative 2xl:mb-10">شهادات واعتمادات</h1>
       </div>
-        <p className="text-right mb-10 2xl:mr-60 2xl:text-xl text-lg ">مصنع نما طيبة حاصل على جميع الشهادات والاعتمادات من الجهات السعودية المختصة</p>
-        <div className="flex justify-center flex-wrap 2xl:gap-44 gap-x-36 gap-20  mt-24 ">
+        <p className=" text-center mb-10 2xl:text-3xl text-2xl ">مصنع نما طيبة حاصل على جميع الشهادات والاعتمادات من الجهات السعودية المختصة</p>
+      <div className="flex justify-center flex-wrap 2xl:gap-48 2xl:gap-x-72 gap-x-52 gap-20  ">
         <img src={hrlogo} alt="image" className="w-52 h-44" />
         <img src={zakalogo} alt="image"  className="w-52 h-44" />
         <img src={energylogo} alt="image"  className="w-52 h-44"/>
@@ -91,7 +90,7 @@ const AboutPage = () => {
         <img src={citylogo} alt="image"  className="w-52 h-44"/>
         <img src={envlogo} alt="image" className="w-52 h-44" />
 
-      </div> */}
+      </div>
       </div>
       <Footer />
     </>
